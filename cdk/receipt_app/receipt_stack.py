e962f32abd8b5c90b40356b7312f8c44ec691a58
cssolutions <fumes06@gmail.com>
2025-07-16 11:41:53 +0000

    Initial project setup

--- cdk/receipt_app/receipt_stack.py
+++ cdk/receipt_app/receipt_stack.py
@@ -1,19 +1,14 @@
+# cdk/receipt_app/receipt_stack.py
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
-class CdkStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+class ReceiptStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # minimal resource so synth/bootstrap can run
+        s3.Bucket(self, "ReceiptUploadBucket")
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
